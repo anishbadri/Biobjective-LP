1b428371ef18b8ac9b2f21a05e165fe7231c0e5c
anishbadri@gmail.com <anishbadri@gmail.com>
2019-04-17 13:03:25 +0530

    Adding float

--- lp/__init__.py
+++ lp/__init__.py
@@ -2,5 +2,6 @@ from flask import Flask
 
 
 app=Flask(__name__)
+app.config['SECRET_KEY'] = 'cac873583f189291c68823d86860459a'
 
 from lp import routes
--- lp/routes.py
+++ lp/routes.py
@@ -7,15 +7,20 @@ from flask import render_template
 
 class UserInputForm(FlaskForm):
     input1 = StringField('Input 1', validators=[DataRequired()])
-    output = None
+    output = 0
     submit = SubmitField('Calculate')
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     # t = bolp([[1.2,1.0,1.1]],[[-64.6,-59.5,-43.9]],[[1,1,1],[432,321,168],[-8,5,8]],[[1000],[320000],[3500]]) 
     # print(t)
     form = UserInputForm()
     if form.validate_on_submit():
-        form.output = int(form.input1.data)*10 
+        mylist = [float(x) for x in form.input1.data.split(',')]
+        out = 0
+        for num in mylist:
+            out += num
+        form.output =  out
         return render_template('index.html', form = form)
     l = bolp([[1.2,1.0,1.1]],[[-64.6,-59.5,-43.9]],[[1,1,1],[432,321,168],[-8,5,8]],[[1000],[320000],[3500]]) 
     return render_template('index.html', form = form)
